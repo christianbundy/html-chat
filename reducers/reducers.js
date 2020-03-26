0479677a3826e905985dd17f3721d437ae9fe590
Cameron Leslie <cameronleslie@gmail.com>
2020-03-25 17:00:32 -0700

    Checking message for no content or lines with only whitespace.

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -5,18 +5,21 @@ const initialStateChat = [
   {user: 'Mr. Computer', message: "Hello you beautiful person!"}
 ];
 
+const isBlankLine = str => RegExp(/^(\s+|\t+)$|(^$)/).test(str);
+
 const chat = (messageState = initialStateChat, action) => {
   switch (action.type) {
     case POST_MESSAGE:
-      // we should do a check on `action.message.message` to check if there are
-      // just a bunch of spaces, easy enough with a regex.
+      // Is it *not* a blank line?
+      if (!isBlankLine(action.message.message)) {
+        // Not blank.
+        return messageState
+          .concat({user: action.message.user, message: action.message.message})
+          // Return last n number of messages
+          .slice(-Math.abs(action.message.chatBufferSize))
+      }
+      // Blank, just perform the default.
       return messageState
-        .concat({
-          user: action.message.user,
-          message: action.message.message
-        })
-        // Return last n number of messages
-        .slice(-Math.abs(action.message.chatBufferSize))
     default:
       return messageState;
   }
